07cfcbf2874584885c73eb32fe5c37ed33bc24e2
sekullar <asdkralxasd@gmail.com>
2025-02-23 15:33:55 +0300

    performance update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite"
+import purgecss from 'vite-plugin-purgecss';
+import { visualizer } from 'rollup-plugin-visualizer';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), purgecss({
+    content: ['./index.html', './src/**/*.jsx', './src/**/*.tsx'], 
+  }),visualizer({ open: true }), ],
 })
